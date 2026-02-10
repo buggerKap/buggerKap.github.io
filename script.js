cfa3dccb68094128d653549a98bd275011cbc952
buggerKap <brasstyler87@gmail.com>
2026-02-10 00:03:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+  // When button is clicked, change its background color to red
+  this.style.backgroundColor = 'red';
+});
